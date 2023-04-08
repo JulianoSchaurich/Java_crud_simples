cd01cf43eadb96d6087f7a75d7741905a791ce27
Juliano Marcelo Schaurich Júnior <schaurich.jmsj@gmail.com>
2023-04-08 10:54:17 -0300

    Add files via upload

---
+++ src/main/java/tarefa_crud/Crud.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tarefa_crud;
+
+/**
+ *
+ * @author Juliano Schaurich
+ */
+public class Crud {
+    public static void main(String[] args) {
+        TesteConexao tela = new TesteConexao();
+        tela.setVisible(true);
+    }
+}
